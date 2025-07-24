4cee3d091c83faeeab907bd2364015ad93196521
Timothy J. McMichael <timmcmic@microsoft.com>
2025-07-23 21:39:56 -0400

    Updating

---
+++ GroupExpirationAudit.ps1
@@ -0,0 +1,48 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID 79d1df22-ec96-4860-b2d4-40dafb649ae1
+
+.AUTHOR timmcmic
+
+.COMPANYNAME
+
+Micorosft CSS
+
+.COPYRIGHT
+
+.TAGS
+
+.LICENSEURI
+
+.PROJECTURI
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+
+
+.PRIVATEDATA
+
+#>
+
+#Requires -Module @{ ModuleName = 'Microsoft.Graph.Authentication'; ModuleVersion = '2.29.1' } 
+#Requires -Module @{ ModuleName = 'Microsoft.Graph.Groups'; ModuleVersion = '2.29.1' }
+#Requires -Module @{ ModuleName = 'PSWriteHTML'; ModuleVersion = '1.30.8' }
+<# 
+
+.DESCRIPTION 
+ This script audits group expiration and provides output information 
+
+#> 
+Param()
+
+
